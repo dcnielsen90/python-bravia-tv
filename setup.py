b9a463b4fc403616a6216beaf461ade65b120040
Dave Nielsen <dcnielsen90@gmail.com>
2020-05-06 08:10:00 -0400

    Clean up publish configs: fixes issue #9

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 import setuptools
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("bravia_tv/__init__.py",'r') as fh:
+    text = fh.read()
+    VERSION = re.search(r'^__version__ *= * "(.*)"$', text, re.M).group(1)
+    MAINTAINER = re.search(r'^__maintainer__ *= * "(.*)"$', text, re.M).group(1)
+    MAINTAINER_EMAIL = re.search(r'^__email__ *= * "(.*)"$', text, re.M).group(1)
+
 setuptools.setup(
-    name="bravia_tv", # Replace with your own username
-    version="1.0.3",
-    maintainer="David Nielsen",
-    maintainer_email="dncielsen90@gmail.com",
+    name="bravia_tv",
+    version=VERSION,
+    maintainer=MAINTAINER,
+    maintainer_email=MAINTAINER_EMAIL,
     description="Python Bravia TV remote control",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/dcnielsen90/python-bravia-tv.git",
+    install_requires=['requests'],
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
